9e109b98c42a5b03c0dce139f3c2d995468ab89c
jiwitesh <jiwitesh05nov@gmail.com>
2020-11-08 17:45:25 +0530

    added the file for running locally without api

--- clientService.py
+++ clientService.py
@@ -1,40 +1,104 @@
 import os
+from wsgiref import simple_server
+from flask import Flask, request, render_template
+from flask import Response
+from flask_cors import CORS, cross_origin
+import shutil, os
+import json
 
-# from com_in_ineuron_ai_service.brandMeasureservice import BrandMeasureService
-# from com_in_ineuron_ai_speech_to_text.speechToText import speech2Text
-from com_in_ineuron_ai_speech_to_text.spetotxt import get_large_audio_transcription
+from com_in_ineuron_ai_speech_to_text.transcriptGenerator import generateTranscript
 from com_in_ineuron_ai_spellingcorrector.spellcorrector import spell_corrector
 from com_in_ineuron_ai_keywordspotter.keywordSpotter import AddMultiKeywords
 
-# brndMsreObj = BrandMeasureService()
-#
-#
-# audio_file_path = 'InputFiles/commercial_mono.wav'
-# brndMsreObj.performSpeakerDiarization(audio_file_path)
+
+app = Flask(__name__)
+CORS(app)
+app.config['DEBUG'] = True
 
 
-FolderPath = "InputFiles"
-fileList = os.listdir(FolderPath)
+class ClientService:
+    def __init__(self):
+        self.FolderPath = "InputFiles"
+        self.fileList = os.listdir(self.FolderPath)
+        self.separatedOutputFiles = "./SeparatedOutputFiles"
+        self.outputText = {}
 
-separatedOutputFiles = "./SeparatedOutputFiles"
-outputText = {}
-for val in fileList:
-    outputText = get_large_audio_transcription(os.path.join(FolderPath,val), separatedOutputFiles)
-print(outputText)
+    def processAudioFile(self):
+        outputResponseObj = {}
+        for val in self.fileList:
+            inputFileTranscriptedOp = generateTranscript(os.path.join(self.FolderPath,val), self.separatedOutputFiles)
+        print(inputFileTranscriptedOp)
+        outputResponseObj["inputFileTranscriptedOp"] = inputFileTranscriptedOp
+
+        spellCorrectedOpMap = {}
+        for val in inputFileTranscriptedOp.keys():
+            spellcorrectedOp = spell_corrector(inputFileTranscriptedOp[val])
+            # print("Input Text : ", outputText[val])
+            # print("Corrected Text : ", spellcorrectedOp)
+            spellCorrectedOpMap[val] = spellcorrectedOp
+            # inputFileTranscriptedOp[val] = spellcorrectedOp
+        outputResponseObj["spellCorrectedOpMap"] = spellCorrectedOpMap
+        print(inputFileTranscriptedOp)
+
+        extractedKeywordMap = {}
+        for val in inputFileTranscriptedOp.keys():
+            adding = AddMultiKeywords(inputFileTranscriptedOp[val],
+                                      {"place": ["england"],
+                                       "team": ["manchester united"],
+                                       "game": ["football"]})
+            result = adding.addkey()
+            extractedKeywordMap[val] = result
+        outputResponseObj["extractedKeywors"] = extractedKeywordMap
+
+        return outputResponseObj
 
 
-for val in outputText.keys():
-    spellcorrectedOp = spell_corrector(outputText[val])
-    # print("Input Text : ", outputText[val])
-    # print("Corrected Text : ", spellcorrectedOp)
-    outputText[val] = spellcorrectedOp
-print(outputText)
+inputFileDir = "./InputFiles"
+archiveDir = './ArchivedInputFiles'
 
-for val in outputText.keys():
-    adding = AddMultiKeywords(outputText[val],
-                              {"place": ["england"],
-                               "team": ["manchester united"],
-                               "game": ["football"]})
-    result = adding.addkey()
-    print(result)
 
+def archiveOldInputFiles():
+    files = os.listdir(inputFileDir)
+    for f in files:
+        if os.path.exists(os.path.join(archiveDir, f)):
+            os.remove(os.path.join(archiveDir, f))
+        shutil.move(os.path.join(inputFileDir, f), archiveDir)
+
+
+@app.route("/", methods=["GET"])
+def homePage():
+    return render_template("index.html")
+
+
+@app.route("/upload", methods=["POST"])
+def getInputFIle():
+    opReap = {}
+    try:
+        inputFile = request.files.get('file')
+        archiveOldInputFiles()
+        inputFile.save(os.path.join("InputFiles", inputFile.filename))
+    except Exception as e:
+        opReap["outputError"] = str(e)
+        jsonStr = json.dumps(opReap, ensure_ascii=False).encode('utf8')
+        return Response(jsonStr.decode())
+    opReap["output"] = "Successfully uploaded the file"
+    jsonStr = json.dumps(opReap, ensure_ascii=False).encode('utf8')
+    return Response(jsonStr.decode())
+
+
+@app.route("/startprocessing", methods=["POST"])
+def processInputFile():
+    opResponseObj = clntApp.processAudioFile()
+    jsonStr = json.dumps(opResponseObj, ensure_ascii=False).encode('utf8')
+    # print(jsonStr.decode())
+    return Response(jsonStr.decode())
+    # return Response()
+
+
+if __name__ == "__main__":
+    clntApp = ClientService()
+    host = '0.0.0.0'
+    port = 5000
+    httpd = simple_server.make_server(host, port, app)
+    print("Serving on %s %d" % (host, port))
+    httpd.serve_forever()
